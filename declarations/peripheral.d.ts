c5b5454d626e488a0c71b43a5b921f2631c94916
Ryan Rossiter <nayr0788@gmail.com>
2021-12-13 20:07:21 -0500

    add monitor peripheral interface

--- declarations/peripheral.d.ts
+++ declarations/peripheral.d.ts
@@ -7,6 +7,39 @@ declare namespace peripheral {
     function getMethods(name: string): string[] | null
     function getName(peripheral: any): string
     function call(name: string, method: string, ...args: unknown[]): any
-    function wrap(name: string): any | null
+    function wrap<T = WrappedPeripheral>(name: string): T | null
     function find(ty: string, filter?: (name: string, wrapped: any) => boolean): any[]
+
+    type WrappedPeripheral = Monitor;
+
+    export interface Monitor {
+        setTextScale(scale :number): void
+        getTextScale(): number
+        write(text: string): void
+        scroll(y: number): void
+        getCursorPos(): LuaMultiReturn<[x: number, y: number]>
+        setCursorPos(x: number, y: number): void
+        getCursorBlink(): boolean
+        setCursorBlink(blink: boolean): void
+        getSize(): LuaMultiReturn<[width: number, height: number]>
+        clear(): void
+        clearLine(): void
+        getTextColor(): number
+        getTextColour(): number
+        setTextColor(color: number): void
+        setTextColour(colour: number): void
+        getBackgroundColor(): number
+        getBackgroundColour(): number
+        setBackgroundColor(color: number): void
+        setBackgroundColour(colour: number): void
+        isColor(): boolean
+        isColour(): boolean
+        blit(text: string, textColor: string, backgroundColor: string): void
+        setPaletteColor(index: number, color: number): void
+        setPaletteColor(index: number, r: number, g: number, b: number): void
+        setPaletteColour(index: number, color: number): void
+        setPaletteColour(index: number, r: number, g: number, b: number): void
+        getPaletteColor(color: number): LuaMultiReturn<[r: number, g: number, b: number]>
+        getPaletteColour(colour: number): LuaMultiReturn<[r: number, g: number, b: number]>
+    }
 }
